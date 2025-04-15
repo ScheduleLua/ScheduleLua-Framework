e326dab01a8ba766f187bf0614dfed76bfc28098
ifBars <tristens5465@gmail.com>
2025-04-14 23:30:26 -0700

    Update Core.cs
    - Change version tag back to 0.1.1 cuz i'm a dummy and put 0.1.2

--- Core.cs
+++ Core.cs
@@ -19,7 +19,7 @@ namespace ScheduleLua;
 public class Core : MelonMod
 {
     // Version constant that can be used in both the MelonInfo attribute and exposed to Lua
-    public const string ModVersion = "0.1.2";
+    public const string ModVersion = "0.1.1";
     
     private static Core _instance;
     public static Core Instance => _instance;
